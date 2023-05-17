5892eea60fc0264f9c4be4c34c50078334de5aeb
PaleVoid <70835432+PaleVoid@users.noreply.github.com>
2023-05-17 14:09:40 +0700

    Update App.js

--- idk/src/App.js
+++ idk/src/App.js
@@ -1,15 +1,17 @@
+import { useState } from "react";
 import useScript from "./api/html_script";
 
 function App()
 {
+  const [ script, setScript ] = useState(null);
   const url = 'https://www.socialintents.com/api/socialintents.1.3.js#2c9fab3588226f81018827b31f8a0597';
 
-  useScript(url);
+  setScript(useScript(url));
 
   return (
     <div className="App">
       <h1>Hello</h1>
-
+      {script}
     </div>
   )
 }
