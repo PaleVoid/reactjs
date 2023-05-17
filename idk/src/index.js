2d562a934875f2a18dfb935c6ecc9a3323b12d71
PaleVoid <70835432+PaleVoid@users.noreply.github.com>
2023-05-17 14:38:11 +0700

    index
    index

--- idk/src/index.js
+++ idk/src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<Router>
+  <div>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </div>
+</Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
