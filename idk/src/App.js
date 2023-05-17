e9cdc664ff2b64f3166f1fd768bada0a724b5868
PaleVoid <70835432+PaleVoid@users.noreply.github.com>
2023-05-17 14:06:34 +0700

    idk
    maybe

--- idk/src/App.js
+++ idk/src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
+import useScript from "./api/html_script";
 
 function App()
 {
-  const number = 5;
+  const url = 'https://www.socialintents.com/api/socialintents.1.3.js#2c9fab3588226f81018827b31f8a0597';
 
-  return(
-    <div>
-      <h1>Hello {number}</h1>
+  useScript(url);
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
 
     </div>
-
   )
-
 }
 
 export default App;
